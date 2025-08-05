24a52ed47df2919f47b4f7b1bff5e4cfdb9ae3aa
edilayehu <edilayehu@eaglelionsystems.com>
2025-08-05 12:06:32 +0300

    typeorm setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'edilayehu',
+      database: 'bookstore',
+      autoLoadEntities: true,
+      synchronize: true, // set to false in production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
